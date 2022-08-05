568f1bbe413d49bbd9c5b86e3b240cbce98296de
Spektrainfiniti <sokol2001-01@mail.ru>
2022-08-05 16:58:09 +0300

    Add files via upload

---
+++ TTPI_decryptor.ps1
@@ -0,0 +1,17 @@
+set-strictMode -version 2.0
+$project_dir = "C:\Users\User\Documents\ps_poligon\";
+$aesManaged=new-object "System.Security.Cryptography.AesManaged";
+$aesManaged.Mode=[System.Security.Cryptography.CipherMode]::CBC;
+$aesManaged.Padding=[System.Security.Cryptography.PaddingMode]::Zeros;
+$aesManaged.BlockSize=128;
+$aesManaged.KeySize=256;
+$IV_enc = [System.IO.File]::ReadAllBytes($project_dir + "iv.txt");
+$key_enc = [System.IO.File]::ReadAllBytes($project_dir + "key.txt");
+
+$data = [System.IO.File]::ReadAllBytes($project_dir + "FLAG.txt.encr");
+
+$decryptor = $aesManaged.CreateDecryptor($key_enc, $IV_enc);
+$new_data = $decryptor.TransformFinalBlock($data, 16, $data.Length - 16);
+
+$result = [System.Text.Encoding]::UTF8.GetString($new_data).Trim([char]0);
+$result > $($project_dir + "RESULT.txt");
